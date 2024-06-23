29f34807d7345089e7757d25e36c5fcbb97575b6
Arunesh Mishra <aruneshm@gmail.com>
2024-06-23 07:14:19 -0700

    git commands

---
+++ git_commands.sh
@@ -0,0 +1,15 @@
+
+# Force push to remote branch (such as Github) in order to force
+# write its history.
+
+git push -f -u origin exp_1
+
+# If you have killed commits using git reset, use git reflog to get back to an older
+# state.
+#
+#
+
+
+# Track a remote branch.
+git remote show origin
+git remote add origin git@github.com:arunesh/createtts.git
